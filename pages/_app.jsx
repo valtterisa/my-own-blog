62590c7eb344e7eb8a5620a5285295fcf343694d
valtteri <savonen.emppu@gmail.com>
2023-02-03 16:40:42 +0200

    added Google Analytics 4

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
-function MyApp( { Component, pageProps }) {
-    return <Component {...pageProps} />
-}
+function App( { Component, pageProps }) {
+    return( 
+    <>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-WW96NP0QD1" />
+        <Script>
+            {`window.dataLayer = window.dataLayer || [];
+            function gtag(){
+                dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
 
-export default MyApp
+            gtag('config', 'G-WW96NP0QD1');
+            `}
+        </Script>
+
+        <Component {...pageProps} />
+    </>
+)}
+
+export default App
