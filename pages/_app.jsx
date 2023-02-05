56840dafc1bae51d69cebce918c5ca85a8ae49d8
valtteri <savonen.emppu@gmail.com>
2023-02-05 19:51:12 +0200

    added google tag manager

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,16 @@ import '../styles/globals.css'
 function App( { Component, pageProps }) {
     return( 
     <>
+        <Script>
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer',${process.env.GTM_TAG});
+            `}
+        </Script>
+        
         {/* Google Analytics 4 */}
         <Script async 
             src="https://www.googletagmanager.com/gtag/js?id=G-WW96NP0QD1" />
---
+++ pages/_document.jsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+        <body>
+            <Main />
+            <NextScript />
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WMLFSW9" height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+        </body>
+    </Html>
+  )
+}
